3ff5c71ee5ea58faa9723d7885e58a30158b124c
ayuha167 <zanma@live.jp>
2013-11-07 17:35:05 +0900

    20131107comment

--- hw.c
+++ hw.c
@@ -1,5 +1,13 @@
+//Ayuha 
 #include<stdio.h>
 
 int main(void){
-    printf("hello world\n");
+    int i,j=0;
+    
+    for (i=0; i<10; i++) {
+        for (j=0; j<i; j++) {
+            printf("¥t");
+        }
+        printf("Kitty on Your Lap ! \n");
+    }
 }
